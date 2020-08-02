71a3c0023d473c0c01460043f2bfbaa087789750
Julesntare <julesntare@gmail.com>
2020-08-03 01:16:50 +0200

    removed firebase configuration in sign up script

--- scripts/firebase-signup.js
+++ scripts/firebase-signup.js
@@ -1,14 +1,3 @@
-const firebaseConfig = {
-	apiKey: 'AIzaSyASc4Oq57XNSMpZ6CgdjXT4AkmroQlX_sY',
-	authDomain: 'contactform-daf99.firebaseapp.com',
-	databaseURL: 'https://contactform-daf99.firebaseio.com',
-	projectId: 'contactform-daf99',
-	storageBucket: 'contactform-daf99.appspot.com',
-	messagingSenderId: '174642545018',
-	appId: '1:174642545018:web:daeb1627427f5995e156c3',
-	measurementId: 'G-WDTGDSYDND',
-};
-
 // reference users collection
 let usersRef = firebase.database().ref('users');
 
