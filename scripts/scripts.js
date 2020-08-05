7b404cc844482b82432402272a652777c990b689
Julesntare <julesntare@gmail.com>
2020-08-05 11:19:05 +0200

    modified scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,17 @@ function toggle(x) {
 	x.classList.toggle('change');
 	elMenu.classList.toggle('change');
 }
+
+firebase.auth().onAuthStateChanged(function (user) {
+	if (user) {
+		//   var displayName = user.displayName;
+		//   var email = user.email;
+		//   var emailVerified = user.emailVerified;
+		//   var photoURL = user.photoURL;
+		//   var isAnonymous = user.isAnonymous;
+		//   var uid = user.uid;
+		//   var providerData = user.providerData;
+		console.log(user);
+	} else {
+	}
+});
