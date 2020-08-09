c5177f27565d8ffcc7c264d4d4058accf2008675
Julesntare <julesntare@gmail.com>
2020-08-09 15:14:21 +0200

    added script for uploading post

--- scripts/firebase-login.js
+++ scripts/firebase-login.js
@@ -12,3 +12,43 @@ document.querySelector('#loginForm').addEventListener('submit', (e) => {
 			console.log(error);
 		});
 });
+
+// fb authentication
+document.querySelector('#fbAuth').addEventListener('click', (e) => {
+	var fbProvider = new firebase.auth.FacebookAuthProvider();
+	firebase
+		.auth()
+		.signInWithPopup(fbProvider)
+		.then((result) => {
+			window.location.href = '../pages/blog.html';
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+// twitter authentication
+document.querySelector('#twitterAuth').addEventListener('click', (e) => {
+	e.preventDefault();
+});
+
+// google authentication
+document.querySelector('#googleAuth').addEventListener('click', (e) => {
+	var googleProvider = new firebase.auth.GoogleAuthProvider();
+	firebase
+		.auth()
+		.signInWithPopup(googleProvider)
+		.then((result) => {
+			window.location.href = '../pages/blog.html';
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+// check auth state
+firebase.auth().onAuthStateChanged(function (user) {
+	if (user) {
+		window.location.href = '../pages/blog.html';
+	}
+});
