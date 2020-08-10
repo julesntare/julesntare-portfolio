222465b13fed8eb870885205b97beee72c058ed0
Julesntare <julesntare@gmail.com>
2020-08-10 07:45:49 +0200

    modified scripts links

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -2,6 +2,8 @@ firebase.initializeApp({
 	apiKey: 'AIzaSyApy6r-ORip9gq8d0EmsckVXcYqlogLAF4',
 	authDomain: 'portfolio-db-d0d5c.firebaseapp.com',
 	projectId: 'portfolio-db-d0d5c',
+	storageBucket: 'gs://portfolio-db-d0d5c.appspot.com',
 });
 
 var db = firebase.firestore();
+var storage = firebase.storage();
