330b8904802c18663af903af0293479ece50a647
Julesntare <julesntare@gmail.com>
2020-08-11 22:09:37 +0200

    added scripts for auth check

--- scripts/userProfile.js
+++ scripts/userProfile.js
@@ -3,45 +3,26 @@ let currPassword = document.querySelector('#curr-password');
 let pswdMsg = document.querySelector('.password-msg');
 let userDetails;
 
+// check auth state
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) {
-		accountName.innerHTML = '@' + user.email.split('@')[0];
-		userDetails = user.email;
-	} else {
-		window.location.href = './login.html';
-	}
-});
-
-// delete account
-document.querySelector('#delete-account').addEventListener('click', (e) => {
-	if (currPassword.value == '') {
-		pswdMsg.style.display = 'block';
-		pswdMsg.innerHTML = 'Please confirm your password';
-	} else {
-		let userDelete = firebase.auth().currentUser;
-		let credential = firebase.auth.EmailAuthProvider.credential(userDetails, currPassword.value);
-		userDelete
-			.reauthenticateWithCredential(credential)
-			.then(() => {
-				userDelete
-					.delete()
-					.then(() => {
-						let deleteDoc = db.collection('users').where('email', '==', userDetails);
-						deleteDoc.get().then((querySnapshot) => {
-							querySnapshot.forEach((doc) => {
-								doc.ref.delete();
-							});
-						});
-						alert('I hope you will come back. bye!!!');
-						window.location.href = './blog.html';
-					})
-					.catch((error) => {
-						console.log(error);
-					});
+		if (accountName) {
+			accountName.innerHTML = '@' + user.email.split('@')[0];
+			userDetails = user.email;
+		}
+		let docRef = db.collection('users').doc(user.uid);
+		docRef
+			.get()
+			.then((doc) => {
+				if (doc.exists && doc.data() && level == 2) {
+					return true;
+				}
+				window.location.href = '../pages/blog.html';
 			})
 			.catch((error) => {
-				pswdMsg.style.display = 'block';
-				pswdMsg.innerHTML = 'Wrong Password';
+				console.log('Error getting document:', error);
 			});
+	} else {
+		window.location.href = '../pages/blog.html';
 	}
 });
